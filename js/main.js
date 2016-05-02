f15dfc6b9a1f9928a4ef70107cbadb4dfed86b5d
chong.isa <chong.isa@gmail.com>
2016-05-02 02:48:34 -0400

    Complete map assignment

--- js/main.js
+++ js/main.js
@@ -19,3 +19,48 @@
 // interesting locations. (You'll need to figure out the latitude/longitude for
 // these locations using a mapping tool such as Google Maps.)
 
+$( document ).ready(function() {
+  $('.dropdown-toggle').dropdown();
+  
+    var lnglat = [46.8523, -121.7603];  
+    var mymap = L.map('mapid').setView(lnglat, 11);
+
+
+		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYW5nZWx1aCIsImEiOiJjaW5wa25pcmcxMDAzdWFtMzhrdmpscnNiIn0.DK0YYY3D2VvBPMnNUhrkvA', {
+			maxZoom: 18,
+			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+				'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+				'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+			id: 'mapbox.streets'
+		}).addTo(mymap);
+
+
+		L.marker(lnglat).addTo(mymap)
+			.bindPopup("<b>Mount Rainier!</b><br />Elevation: 14,411 ft.").openPopup();
+
+		L.circle(lnglat, 500, {
+			color: 'red',
+			fillColor: '#f03',
+			fillOpacity: 0.5
+		}).addTo(mymap).bindPopup("Hike Paradise");
+
+		L.polygon([
+			[46.7523, -121.7803],
+			lnglat,
+      [46.86, -121.6]
+		]).addTo(mymap).bindPopup("Campgrounds near Tatoosh/Cowlitz.");
+
+
+		var popup = L.popup();
+
+		function onMapClick(e) {
+			popup
+				.setLatLng(e.latlng)
+				.setContent("You clicked the map at " + e.latlng.toString())
+				.openOn(mymap);
+		}
+
+		mymap.on('click', onMapClick);
+
+  
+});
